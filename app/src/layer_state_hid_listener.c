80f76c00c04f120d69a21a6ca721c2a3f11ae843
xkonni <xkonni@posteo.de>
2026-06-05 13:24:17 +0200

    fixup

--- app/src/layer_state_hid_listener.c
+++ app/src/layer_state_hid_listener.c
@@ -6,17 +6,22 @@
 
 #include <zmk/event_manager.h>
 #include <zmk/events/layer_state_changed.h>
+#include <zmk/events/endpoint_changed.h>
 #include <zmk/hid.h>
 #include <zmk/endpoints.h>
 #include <zmk/keymap.h>
 
-static int layer_state_hid_listener(const zmk_event_t *eh) {
+static void send_current_layer_state(void) {
     zmk_keymap_layer_index_t index = zmk_keymap_highest_layer_active();
-    zmk_hid_layer_state_set(index, zmk_keymap_layer_locked(
-        zmk_keymap_layer_index_to_id(index)) ? 1 : 0);
+    zmk_hid_layer_state_set(index, 0);
     zmk_endpoint_send_layer_state_report();
+}
+
+static int layer_state_hid_listener(const zmk_event_t *eh) {
+    send_current_layer_state();
     return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(layer_state_hid_listener, layer_state_hid_listener);
 ZMK_SUBSCRIPTION(layer_state_hid_listener, zmk_layer_state_changed);
+ZMK_SUBSCRIPTION(layer_state_hid_listener, zmk_endpoint_changed);
